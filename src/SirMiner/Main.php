3e5c34ddec111563d4f3e6ce9709edf7c3a6363a
SirMiner <sirminerpe@gmail.com>
2017-02-22 12:00:12 -0800

    Fly Command

--- src/SirMiner/Main.php
+++ src/SirMiner/Main.php
@@ -11,18 +11,28 @@ use pocketmine\command\ConsoleCommandSender;
 use pocketmine\Player;
 use pocketmine\Server;
 
-
-
 class Main extends PluginBase implements Listener{
 
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getServer()->getLogger()->notice(TF::AQUA . "EnchantPlus Enabled!");
+    $this->getServer()->getLogger()->notice(TF::AQUA . "VIPCommands Enabled!");
   }
   
 public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
       		switch (strtolower($cmd->getName())) {
 				case "fly":
+				if (isset($args[0]) && strtolower($args[0]) == "on") {
+                  if($sender->hasPermission("vip.fly")) {
+                            $sender->sendMessage(TF::GREEN ."You can now fly!");
+                            $sender->setAllowFlight(TRUE);
+                }
+                }
+                if (isset($args[0]) && strtolower($args[0]) == "off") {
+                  if($sender->hasPermission("vip.fly")) {
+                            $sender->sendMessage(TF::RED . "You disabled fly!");
+                            $sender->setAllowFlight(FALSE);
+                }
+                }
 				break;
    }
    }
